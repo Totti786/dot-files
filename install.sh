5efa80d9e25fb8875ff4577e1d9237063cf9d6be
Totti <tarekdshli@gmail.com>
2022-05-27 05:35:25 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,11 +2,10 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
-#sudo cp -r /$DIR/.scripts/bin/ /usr/local/
-
+sudo cp -r /$DIR/.scripts/bin/ /usr/local/
 cp -r /$DIR/.scripts/ ~/
-#cp -r /$DIR/.config/ ~/.config
-#cp -r /$DIR/.local/ ~/.local
+cp -r /$DIR/.config/ ~/.config
+cp -r /$DIR/.local/ ~/.local
 cp -r /$DIR/.ncmpcpp/ ~/
 cp -r /$DIR/.oh-my-zsh/ ~/
 cp /$DIR/.zshrc ~/
