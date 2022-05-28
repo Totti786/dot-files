a79d776e91e905963ce56709886cb3598a19375f
Totti <tarekdshli@gmail.com>
2022-05-28 17:43:31 +0300

    small fixes

--- .scripts/mkvedit.sh
+++ .scripts/mkvedit.sh
@@ -3,6 +3,13 @@
 # This script takes all mkv files in the current directory and sets the filename
 # (without .mkv) as its title in metadata
 
-for mkvfile in *.mkv; do
-        mkvpropedit "$mkvfile" -e info -s title="${mkvfile%.mkv}"
-    done
+
+if command -v mkvpropedit; then 
+	for mkvfile in *.mkv; do
+	        mkvpropedit "$mkvfile" -e info -s title="${mkvfile%.mkv}"
+	    done
+else 
+	echo "mkvtoolnix not found"
+	echo "Installing..."
+	yay -S mkvtoolnix-cli
+fi
--- .scripts/yay.sh
+++ .scripts/yay.sh
@@ -3,15 +3,11 @@
 yay(){
 if command -v git &> /dev/null; then
 	echo "git is found, continuing with the installation..."
-	cd /opt
-	sudo git clone https://aur.archlinux.org/yay-git.git
-	echo "Enter your username" && read username
-	echo "Your user name is $username"
-	sudo chown -R $username:$username ./yay-git && cd yay-git
+	git clone https://aur.archlinux.org/yay-git.git
+	cd yay-git
 	makepkg -si && echo "yay installed successfully"
-	cd
 else 
-	sudo pacman -S git && yay
+	sudo pacman -S --needed base-devel git && yay
 fi	
 }
 
