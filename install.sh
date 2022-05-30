838464298dc97d5aee073c662d8af2e61d57e3cf
Totti <tarekdshli@gmail.com>
2022-05-31 01:37:19 +0300

    small fixes

--- install.sh
+++ install.sh
@@ -2,10 +2,21 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
-sudo cp -r /$DIR/.scripts/bin/ /usr/local/
-cp -r /$DIR/.scripts/ ~/
-cp -r /$DIR/.config/ ~/.config
-cp -r /$DIR/.local/ ~/.local
-cp -r /$DIR/.ncmpcpp/ ~/
-cp -r /$DIR/.oh-my-zsh/ ~/
-cp /$DIR/.zshrc ~/
+dirs=(Documents Music Pictures Pictures/Screenshots Videos Downloads Desktop)
+for dir in "${dirs[@]}"; do
+	if [ -d "$HOME/$dir" ]; then
+		echo "fuck"
+	else 
+		echo "shit"
+		mkdir $HOME/$dir
+	fi
+done
+
+
+
+#sudo cp -r /$DIR/.scripts/bin/ /usr/local/ && echo "moved bin to /usr/local"
+#cp -r /$DIR/.scripts/ ~/ && echo "moved scripts home" 
+#cp -r /$DIR/.config/ ~/.config && echo ""
+#cp -r /$DIR/.local/ ~/.local && echo ""
+#cp -r /$DIR/.ncmpcpp/ ~/ && echo "moved nvmpcpp home"
+#cp /$DIR/.zshrc ~/ && echo ""
