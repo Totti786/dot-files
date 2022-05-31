e44b1fe6934dfa0c105b982ecfc80fd5938e07d6
Totti <tarekdshli@gmail.com>
2022-05-31 05:57:32 +0300

    Update sddm.sh

--- sddm/sddm.sh
+++ sddm/sddm.sh
@@ -4,9 +4,10 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 theme="plasma-chili"
 
 if command -v sddm &> /dev/null; then
-	#sudo cp -r $DIR/$theme /usr/share/sddm/themes/ &&
+	yay -S plasma-framework &&
+	sudo cp -r $DIR/$theme /usr/share/sddm/themes/ &&
 	sudo mkdir /etc/sddm.conf.d/
-	sudo cp $DIR/theme.conf /etc/sddm.conf.d/
+	sudo cp $DIR/theme.conf /etc/sddm.conf.d/ &&
 	sed -i -e "s/Current=.*/Current=$theme/g" /etc/sddm.conf.d/theme.conf
 fi	
 
