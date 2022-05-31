c42c1546912eeb9a4e217fd170b4d38396e38f51
Totti <tarekdshli@gmail.com>
2022-05-31 14:40:51 +0300

    Update yay.sh

--- .scripts/yay.sh
+++ .scripts/yay.sh
@@ -5,7 +5,7 @@ if command -v git &> /dev/null; then
 	echo "git is found, continuing with the installation..."
 	git clone https://aur.archlinux.org/yay-git.git
 	cd yay-git
-	makepkg -si && echo "yay installed successfully"
+	sudo makepkg -si && echo "yay installed successfully"
 else 
 	sudo pacman -S --needed base-devel git && yay
 fi	
