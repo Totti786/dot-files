28ca279de25df7afab970a2b017fdebd2ae174c0
Totti <tarekdshli@gmail.com>
2022-05-31 13:48:32 +0300

    Update sddm.sh

--- sddm/sddm.sh
+++ sddm/sddm.sh
@@ -8,16 +8,13 @@ if command -v sddm &> /dev/null; then
 	if [ -d "/usr/share/sddm/themes/$theme" ]; then  
 		if [ -f "/etc/sddm.conf.d/theme.conf" ];then
 			sudo sed -i -e "s/Current=.*/Current=$theme/g" /etc/sddm.conf.d/theme.conf
-			echo "shit"
 		else
 			sudo mkdir /etc/sddm.conf.d/ &&
 			sudo cp $DIR/theme.conf /etc/sddm.conf.d/ &&
 			sudo sed -i -e "s/Current=.*/Current=$theme/g" /etc/sddm.conf.d/theme.conf
-			echo "cock"
 		fi
 	else 
 		sudo cp -r $DIR/$theme /usr/share/sddm/themes/ &&
-		echo "fuck"
 		main
 	fi
 else
