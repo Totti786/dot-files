32a9f0763b33bd9ae17cdae5eb090ece54000f4d
Totti <tarekdshli@gmail.com>
2022-05-31 03:56:55 +0300

    Update plymouth.sh

--- plymouth/plymouth.sh
+++ plymouth/plymouth.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
+main(){
 if command -v plymouth; then 
 	sudo cp -r deus_ex /usr/share/plymouth/themes/
 	
 	sudo plymouth-set-default-theme -l
 	# now set the theme (angular, in this case) and rebuilt the initrd
 	sudo plymouth-set-default-theme -R angu
+else 
+	sudo pacman -S plymouth && 
+	main	
 fi
+}
+
+main
