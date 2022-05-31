851069f95dc20309206bdbdea2b0b7957e7e5745
Totti <tarekdshli@gmail.com>
2022-05-31 13:48:22 +0300

    Update sddm.sh

--- sddm/sddm.sh
+++ sddm/sddm.sh
@@ -5,18 +5,25 @@ theme="plasma-chili"
 
 main(){
 if command -v sddm &> /dev/null; then
-	sudo cp -r $DIR/$theme /usr/share/sddm/themes/ &&
-	if [ -f /etc/sddm.conf.d/theme.conf ];then
-		sed -i -e "s/Current=.*/Current=$theme/g" /etc/sddm.conf.d/theme.conf
-	else
-		sudo mkdir /etc/sddm.conf.d/ &&
-		sudo cp $DIR/theme.conf /etc/sddm.conf.d/ &&
-		sudo sed -i -e "s/Current=.*/Current=$theme/g" /etc/sddm.conf.d/theme.conf
-
+	if [ -d "/usr/share/sddm/themes/$theme" ]; then  
+		if [ -f "/etc/sddm.conf.d/theme.conf" ];then
+			sudo sed -i -e "s/Current=.*/Current=$theme/g" /etc/sddm.conf.d/theme.conf
+			echo "shit"
+		else
+			sudo mkdir /etc/sddm.conf.d/ &&
+			sudo cp $DIR/theme.conf /etc/sddm.conf.d/ &&
+			sudo sed -i -e "s/Current=.*/Current=$theme/g" /etc/sddm.conf.d/theme.conf
+			echo "cock"
+		fi
+	else 
+		sudo cp -r $DIR/$theme /usr/share/sddm/themes/ &&
+		echo "fuck"
+		main
 	fi
 else
 	yay -S sddm plasma-framework && 
-	sudo systemctl enable sddm.service
+	sudo systemctl enable sddm.service && 
+	main
 fi	
 }
 main
