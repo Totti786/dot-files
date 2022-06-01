679ed3b31e2cc54dfad0baef2ca7e47e38c6b5db
Totti <tarekdshli@gmail.com>
2022-06-01 21:25:21 +0300

    fixed sddm sh

--- sddm/sddm.sh
+++ sddm/sddm.sh
@@ -24,12 +24,4 @@ else
 fi	
 }
 
-
-#if [[ "$(grep 'ExecStart=' /etc/systemd/system/display-manager.service)"  = "ExecStart=/usr/bin/lightdm" ]]; then
-	sudo systemctl disable lightdm.service &&
-	#sudo pacman -R lightdm-gtk-greeter ligthdm
-	main
-#else
-	#main
-#fi
-
+main
