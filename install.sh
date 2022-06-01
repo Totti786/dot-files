e58790f4fcf5e2737a126281094753a7e6f2dde0
Totti <tarekdshli@gmail.com>
2022-06-02 00:06:52 +0300

    added an icon to sddm

--- install.sh
+++ install.sh
@@ -18,7 +18,7 @@ zsh(){
 		cp -r /$DIR/.zshrc ~/
 		echo "Zsh should be working now"
 	else 
-		echo "Eroor"
+		echo "Error"
 	fi
 	}
 
--- sddm/sddm.sh
+++ sddm/sddm.sh
@@ -8,10 +8,12 @@ if command -v sddm &> /dev/null; then
 	if [ -d "/usr/share/sddm/themes/$theme" ]; then  
 		if [ -f "/etc/sddm.conf.d/theme.conf" ];then
 			sudo sed -i -e "s/Current=.*/Current=$theme/g" /etc/sddm.conf.d/theme.conf
+			sudo cp -r $DIR/sddm/.face.icon /usr/share/sddm/faces 
 		else
 			sudo mkdir /etc/sddm.conf.d/ &&
 			sudo cp $DIR/theme.conf /etc/sddm.conf.d/ &&
 			sudo sed -i -e "s/Current=.*/Current=$theme/g" /etc/sddm.conf.d/theme.conf
+			sudo cp -r $DIR/sddm/.face.icon /usr/share/sddm/faces
 		fi
 	else 
 		sudo cp -r $DIR/$theme /usr/share/sddm/themes/ &&
