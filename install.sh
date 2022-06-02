99539db665c0b74130f242f2059aa4a6674d3a76
Totti <tarekdshli@gmail.com>
2022-06-03 01:27:53 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -14,18 +14,19 @@ xdg-user-dirs-update
 
 zsh(){
 	if command -v zsh &> /dev/null; then
-		if [ ! -d $HOME/.oh-my-zsh ]; then
-			sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" && 
-			zsh
-		fi
-		#chsh -s $(which zsh) &&
-		cp -r $DIR/zsh/custom ~/.oh-my-zsh/ && echo "moved zsh custom files" &&
-		git clone https://github.com/zsh-users/zsh-autosuggestions.git ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions &&
-		cp -r $DIR/.zshrc ~/ &&
-		echo "Zsh should be working now" 
+		echo "wtf is this"
+		#if [ ! -d $HOME/.oh-my-zsh ]; then
+			#sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" && 
+			#zsh
+		#fi
+		##chsh -s $(which zsh) &&
+		#cp -r $DIR/zsh/custom ~/.oh-my-zsh/ && echo "moved zsh custom files" &&
+		#git clone https://github.com/zsh-users/zsh-autosuggestions.git ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions &&
+		#cp -r $DIR/.zshrc ~/ &&
+		#echo "Zsh should be working now" 
 	else 
-		echo "Zsh is not installed installing..." &&
-		sudo pacman -S zsh &&
+		echo "Zsh is not installed installing..." 
+		sudo pacman -S zsh 
 		zsh
 	fi
 	}
