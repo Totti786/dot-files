6c81f36c3ef2e342aaadb803b1a5041497874768
Totti <tarekdshli@gmail.com>
2022-06-03 01:12:57 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -13,25 +13,26 @@ xdg-user-dirs-update
 }
 
 zsh(){
-	if command -v zsh &> /dev/null && [ ! -d $HOME/.oh-my-zsh ]; then 
-		yay -S zsh &&
-		chsh -s $(which zsh) &&
-		cp /$DIR/.zshrc ~/ && 
-		sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" &&
-		cp -r /$DIR/zsh/custom ~/.oh-my-zsh/ && echo "moved zsh custom files" &&
+	if command -v zsh &> /dev/null; then
+		if [ ! -d $HOME/.oh-my-zsh ]; then
+			sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+		fi
+		#chsh -s $(which zsh) &&
+		cp -r $DIR/zsh/custom ~/.oh-my-zsh/ && echo "moved zsh custom files" &&
 		git clone https://github.com/zsh-users/zsh-autosuggestions.git ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions &&
-		cp -r /$DIR/.zshrc ~/ &&
+		cp -r $DIR/.zshrc ~/ &&
 		echo "Zsh should be working now" 
 	else 
-		echo "make sure that zsh is installed"
+		echo "Zsh is not installed, installing... "
+		yay -S zsh
 	fi
 	}
 
 moveConfigs(){
-	cp -r /$DIR/.scripts/ ~/ && echo "moved scripts home" 
-	cp -r /$DIR/.config/ ~/ && echo "moved config files"
-	cp -r /$DIR/.local/ ~/ && echo "moved fonts and icons"
-	sudo cp -r /$DIR/.scripts/bin/ /usr/local/ && echo "moved bin to /usr/local"
+	cp -r $DIR/.scripts/ ~/ && echo "moved scripts home" 
+	cp -r $DIR/.config/ ~/ && echo "moved config files"
+	cp -r $DIR/.local/ ~/ && echo "moved fonts and icons"
+	sudo cp -r $DIR/.scripts/bin/ /usr/local/ && echo "moved bin to /usr/local"
 	}
 
 minimal(){
