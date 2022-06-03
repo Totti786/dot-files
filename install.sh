9042f74ab715b7f0f871a4a5e23809ca0071b7c1
Totti <tarekdshli@gmail.com>
2022-06-03 04:19:55 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -18,7 +18,7 @@ zsh(){
 			sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" && 
 			zsh
 		fi
-		chsh -s $(which zsh) &&
+		#chsh -s $(which zsh) &&
 		cp -r $DIR/zsh/custom ~/.oh-my-zsh/ && echo "moved zsh custom files" &&
 		git clone https://github.com/zsh-users/zsh-autosuggestions.git ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions &&
 		cp -r $DIR/.zshrc ~/ &&
