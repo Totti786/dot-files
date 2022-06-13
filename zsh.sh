d5790bd01f52538d2828217659d293981f2cd518
Totti <tarekdshli@gmail.com>
2022-06-13 18:05:04 +0300

    fixed zsh

--- zsh.sh
+++ zsh.sh
@@ -4,10 +4,9 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
 zsh(){
 	if pacman -Q zsh &> /dev/null; then
-	echo "zsh is installed"
+		echo "zsh is installed"
 		if [ ! -d $HOME/.oh-my-zsh ]; then
-			git clone https://github.com/ohmyzsh/ohmyzsh.git ~/.oh-my-zsh &&
-			zsh
+			git clone https://github.com/ohmyzsh/ohmyzsh.git ~/.oh-my-zsh
 		fi
 		chsh -s $(which zsh) &&
 		cp -r $DIR/zsh/custom/ ~/.oh-my-zsh/ && echo "moved zsh custom files" &&
