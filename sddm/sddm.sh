19ab5af3d54bca3cd92c90a0c410f3662e3fc96d
Totti <tarekdshli@gmail.com>
2022-06-15 18:22:46 +0300

    Update sddm.sh

--- sddm/sddm.sh
+++ sddm/sddm.sh
@@ -8,7 +8,8 @@ if command -v sddm &> /dev/null; then
 	if [ -d "/usr/share/sddm/themes/$theme" ]; then  
 		if [ -f "/etc/sddm.conf.d/theme.conf" ];then
 			sudo sed -i -e "s/Current=.*/Current=$theme/g" /etc/sddm.conf.d/theme.conf
-			sudo cp -r $DIR/.face.icon /usr/share/sddm/faces 
+			sudo cp -r $DIR/.face.icon ~/ 
+			sudo ln ~/.face.icon /usr/share/sddm/faces/.face.icon
 		else
 			sudo mkdir /etc/sddm.conf.d/ &&
 			sudo cp $DIR/theme.conf /etc/sddm.conf.d/ &&
