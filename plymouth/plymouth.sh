48776ebcf2394417205ad49775fc6e8e18f6f529
Totti <tarekdshli@gmail.com>
2022-06-18 21:59:20 +0300

    Update plymouth.sh

--- plymouth/plymouth.sh
+++ plymouth/plymouth.sh
@@ -4,7 +4,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
 main(){
 if command -v plymouth &> /dev/null; then 
-	sudo cp -r deus_ex /usr/share/plymouth/themes/
+	sudo cp -r $DIR/deus_ex /usr/share/plymouth/themes/
 	sudo plymouth-set-default-theme -l
 	# now set the theme (deus_ex, in this case) and rebuilt the initrd
 	sudo plymouth-set-default-theme -R deus_ex
