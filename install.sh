e0977bda622843ee58505ae60b319d68f4c71ac9
Totti <tarekdshli@gmail.com>
2022-06-19 13:57:06 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -22,7 +22,7 @@ changeTheme(){
 	xfconf-query -c xsettings -p /Net/ThemeName -s "FlatColor"
 	xfconf-query -c xsettings -p /Net/IconThemeName -s "Luna-Dark"	
 	xfconf-query -c displays -p\ 
-	/Default/$(xrandr | grep -w connected | awk '{print $1}')/Reflection -s "1920x1080"
+	/Default/$(xrandr | grep -w connected | awk '{print $1}')/Resolution -s "1920x1080"
 	cp -r $DIR/.icons ~/
 	}
 
